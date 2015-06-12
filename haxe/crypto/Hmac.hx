9957f6bf392d454cdf9888d82ec1d11dbd066d46
vroad <xvroad@gmail.com>
2015-06-12 20:54:37 +0900

    Move haxe.crypto.HashMethod to another file

--- haxe/crypto/Hmac.hx
+++ haxe/crypto/Hmac.hx
@@ -22,15 +22,6 @@
 package haxe.crypto;
 
 /**
-    Hash methods for Hmac calculation.
-*/
-enum HashMethod {
-	MD5;
-	SHA1;
-	SHA256;
-}
-
-/**
     Calculates a Hmac of the given Bytes using a HashMethod.
 */
 class Hmac {
