64ba63f54fd536f56cbec66905d726f411df9418
vroad <xvroad@gmail.com>
2015-07-19 12:33:44 +0900

    Disable OpenGL functions that do not exist in OpenGL ES 2.0

--- project/src/graphics/opengl/OpenGLExtensions.h
+++ project/src/graphics/opengl/OpenGLExtensions.h
@@ -44,19 +44,11 @@
    #define OGL_EXT(func,ret,args) \
    {\
       *(void **)&lime::func = (void *)SDL_GL_GetProcAddress(#func);\
-      if (!func) \
-         printf ("*" #func "\n");\
-      else\
-         printf (#func "\n");\
    }
    
    #define EGL_EXT(func,ret,args) \
    {\
       *(void **)&lime::func = (void *)GetProcAddress((HMODULE)lime::OpenGLBindings::eglHandle, #func);\
-      if (!func) \
-         printf ("*" #func "\n");\
-      else\
-         printf (#func "\n");\
    }
 #elif LIME_SDL
    #define OGL_EXT(func,ret,args) \
@@ -184,21 +176,21 @@ OGL_EXT(glVertexAttrib4fv,void,(GLuint, const GLfloat *));
 #ifdef DYNAMIC_OGL
 
 //OGL_EXT(glActiveTexture,void, (GLenum texture));
-OGL_EXT(glAlphaFunc,void, (GLenum func, GLclampf ref));
+//OGL_EXT(glAlphaFunc,void, (GLenum func, GLclampf ref));
 //OGL_EXT(glAlphaFuncx,void, (GLenum func, GLclampx ref));
 OGL_EXT(glBindTexture,void, (GLenum target, GLuint texture));
 OGL_EXT(glBlendFunc,void, (GLenum sfactor, GLenum dfactor));
 OGL_EXT(glClear,void, (GLbitfield mask));
 OGL_EXT(glClearColor,void, (GLclampf red, GLclampf green, GLclampf blue, GLclampf alpha));
 //OGL_EXT(glClearColorx,void, (GLclampx red, GLclampx green, GLclampx blue, GLclampx alpha));
-OGL_EXT(glClearDepth,void, (GLclampd depth));
+//OGL_EXT(glClearDepth,void, (GLclampd depth));
 //OGL_EXT(glClearDepthx,void, (GLclampx depth));
 OGL_EXT(glClearStencil,void, (GLint s));
-OGL_EXT(glClientActiveTexture,void, (GLenum texture));
-OGL_EXT(glColor4f,void, (GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha));
+//OGL_EXT(glClientActiveTexture,void, (GLenum texture));
+//OGL_EXT(glColor4f,void, (GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha));
 //OGL_EXT(glColor4x,void, (GLfixed red, GLfixed green, GLfixed blue, GLfixed alpha));
 OGL_EXT(glColorMask,void, (GLboolean red, GLboolean green, GLboolean blue, GLboolean alpha));
-OGL_EXT(glColorPointer,void, (GLint size, GLenum type, GLsizei stride, const GLvoid *pointer));
+//OGL_EXT(glColorPointer,void, (GLint size, GLenum type, GLsizei stride, const GLvoid *pointer));
 //OGL_EXT(glCompressedTexImage2D,void, (GLenum target, GLint level, GLenum internalformat, GLsizei width, GLsizei height, GLint border, GLsizei imageSize, const GLvoid *data));
 //OGL_EXT(glCompressedTexSubImage2D,void, (GLenum target, GLint level, GLint xoffset, GLint yoffset, GLsizei width, GLsizei height, GLenum format, GLsizei imageSize, const GLvoid *data));
 OGL_EXT(glCopyTexImage2D,void, (GLenum target, GLint level, GLenum internalformat, GLint x, GLint y, GLsizei width, GLsizei height, GLint border));
@@ -207,22 +199,22 @@ OGL_EXT(glCullFace,void, (GLenum mode));
 OGL_EXT(glDeleteTextures,void, (GLsizei n, const GLuint *textures));
 OGL_EXT(glDepthFunc,void, (GLenum func));
 OGL_EXT(glDepthMask,void, (GLboolean flag));
-OGL_EXT(glDepthRange,void, (GLclampd zNear, GLclampd zFar));
+//OGL_EXT(glDepthRange,void, (GLclampd zNear, GLclampd zFar));
 //OGL_EXT(glDepthRangex,void, (GLclampx zNear, GLclampx zFar));
 OGL_EXT(glDisable,void, (GLenum cap));
-OGL_EXT(glDisableClientState,void, (GLenum array));
+//OGL_EXT(glDisableClientState,void, (GLenum array));
 OGL_EXT(glDrawArrays,void, (GLenum mode, GLint first, GLsizei count));
 OGL_EXT(glDrawElements,void, (GLenum mode, GLsizei count, GLenum type, const GLvoid *indices));
 OGL_EXT(glEnable,void, (GLenum cap));
-OGL_EXT(glEnableClientState,void, (GLenum array));
+//OGL_EXT(glEnableClientState,void, (GLenum array));
 OGL_EXT(glFinish,void, (void));
 OGL_EXT(glFlush,void, (void));
-OGL_EXT(glFogf,void, (GLenum pname, GLfloat param));
-OGL_EXT(glFogfv,void, (GLenum pname, const GLfloat *params));
+//OGL_EXT(glFogf,void, (GLenum pname, GLfloat param));
+//OGL_EXT(glFogfv,void, (GLenum pname, const GLfloat *params));
 //OGL_EXT(glFogx,void, (GLenum pname, GLfixed param));
 //OGL_EXT(glFogxv,void, (GLenum pname, const GLfixed *params));
 OGL_EXT(glFrontFace,void, (GLenum mode));
-OGL_EXT(glFrustumf,void, (GLfloat left, GLfloat right, GLfloat bottom, GLfloat top, GLfloat zNear, GLfloat zFar));
+//OGL_EXT(glFrustumf,void, (GLfloat left, GLfloat right, GLfloat bottom, GLfloat top, GLfloat zNear, GLfloat zFar));
 //OGL_EXT(glFrustumx,void, (GLfixed left, GLfixed right, GLfixed bottom, GLfixed top, GLfixed zNear, GLfixed zFar));
 OGL_EXT(glGenTextures,void, (GLsizei n, GLuint *textures));
 OGL_EXT(glGetError,GLenum, (void));
@@ -230,65 +222,65 @@ OGL_EXT(glGetFloatv,void, (GLenum pname, GLfloat* params));
 OGL_EXT(glGetIntegerv,void, (GLenum pname, GLint *params));
 OGL_EXT(glGetString, const GLubyte *,(GLenum name));
 OGL_EXT(glHint,void, (GLenum target, GLenum mode));
-OGL_EXT(glLightModelf,void, (GLenum pname, GLfloat param));
-OGL_EXT(glLightModelfv,void, (GLenum pname, const GLfloat *params));
+//OGL_EXT(glLightModelf,void, (GLenum pname, GLfloat param));
+//OGL_EXT(glLightModelfv,void, (GLenum pname, const GLfloat *params));
 //OGL_EXT(glLightModelx,void, (GLenum pname, GLfixed param));
 //OGL_EXT(glLightModelxv,void, (GLenum pname, const GLfixed *params));
-OGL_EXT(glLightf,void, (GLenum light, GLenum pname, GLfloat param));
-OGL_EXT(glLightfv,void, (GLenum light, GLenum pname, const GLfloat *params));
+//OGL_EXT(glLightf,void, (GLenum light, GLenum pname, GLfloat param));
+//OGL_EXT(glLightfv,void, (GLenum light, GLenum pname, const GLfloat *params));
 //OGL_EXT(glLightx,void, (GLenum light, GLenum pname, GLfixed param));
 //OGL_EXT(glLightxv,void, (GLenum light, GLenum pname, const GLfixed *params));
 OGL_EXT(glLineWidth,void, (GLfloat width));
 //OGL_EXT(glLineWidthx,void, (GLfixed width));
-OGL_EXT(glLoadIdentity,void, (void));
-OGL_EXT(glLoadMatrixf,void, (const GLfloat *m));
+//OGL_EXT(glLoadIdentity,void, (void));
+//OGL_EXT(glLoadMatrixf,void, (const GLfloat *m));
 //OGL_EXT(glLoadMatrixx,void, (const GLfixed *m));
-OGL_EXT(glLogicOp,void, (GLenum opcode));
-OGL_EXT(glMaterialf,void, (GLenum face, GLenum pname, GLfloat param));
-OGL_EXT(glMaterialfv,void, (GLenum face, GLenum pname, const GLfloat *params));
+//OGL_EXT(glLogicOp,void, (GLenum opcode));
+//OGL_EXT(glMaterialf,void, (GLenum face, GLenum pname, GLfloat param));
+//OGL_EXT(glMaterialfv,void, (GLenum face, GLenum pname, const GLfloat *params));
 //OGL_EXT(glMaterialx,void, (GLenum face, GLenum pname, GLfixed param));
 //OGL_EXT(glMaterialxv,void, (GLenum face, GLenum pname, const GLfixed *params));
-OGL_EXT(glMatrixMode,void, (GLenum mode));
-OGL_EXT(glMultMatrixf,void, (const GLfloat *m));
+//OGL_EXT(glMatrixMode,void, (GLenum mode));
+//OGL_EXT(glMultMatrixf,void, (const GLfloat *m));
 //OGL_EXT(glMultMatrixx,void, (const GLfixed *m));
-OGL_EXT(glMultiTexCoord4f,void, (GLenum target, GLfloat s, GLfloat t, GLfloat r, GLfloat q));
+//OGL_EXT(glMultiTexCoord4f,void, (GLenum target, GLfloat s, GLfloat t, GLfloat r, GLfloat q));
 //OGL_EXT(glMultiTexCoord4x,void, (GLenum target, GLfixed s, GLfixed t, GLfixed r, GLfixed q));
-OGL_EXT(glNormal3f,void, (GLfloat nx, GLfloat ny, GLfloat nz));
+//OGL_EXT(glNormal3f,void, (GLfloat nx, GLfloat ny, GLfloat nz));
 //OGL_EXT(glNormal3x,void, (GLfixed nx, GLfixed ny, GLfixed nz));
-OGL_EXT(glNormalPointer,void, (GLenum type, GLsizei stride, const GLvoid *pointer));
-OGL_EXT(glOrthof,void, (GLfloat left, GLfloat right, GLfloat bottom, GLfloat top, GLfloat zNear, GLfloat zFar));
+//OGL_EXT(glNormalPointer,void, (GLenum type, GLsizei stride, const GLvoid *pointer));
+//OGL_EXT(glOrthof,void, (GLfloat left, GLfloat right, GLfloat bottom, GLfloat top, GLfloat zNear, GLfloat zFar));
 //OGL_EXT(glOrthox,void, (GLfixed left, GLfixed right, GLfixed bottom, GLfixed top, GLfixed zNear, GLfixed zFar));
 OGL_EXT(glPixelStorei,void, (GLenum pname, GLint param));
-OGL_EXT(glPointSize,void, (GLfloat size));
+//OGL_EXT(glPointSize,void, (GLfloat size));
 //OGL_EXT(glPointSizex,void, (GLfixed size));
 OGL_EXT(glPolygonOffset,void, (GLfloat factor, GLfloat units));
 //OGL_EXT(glPolygonOffsetx,void, (GLfixed factor, GLfixed units));
-OGL_EXT(glPopMatrix,void, (void));
-OGL_EXT(glPushMatrix,void, (void));
+//OGL_EXT(glPopMatrix,void, (void));
+//OGL_EXT(glPushMatrix,void, (void));
 OGL_EXT(glReadPixels,void, (GLint x, GLint y, GLsizei width, GLsizei height, GLenum format, GLenum type, GLvoid *pixels));
-OGL_EXT(glRotatef,void, (GLfloat angle, GLfloat x, GLfloat y, GLfloat z));
+//OGL_EXT(glRotatef,void, (GLfloat angle, GLfloat x, GLfloat y, GLfloat z));
 //OGL_EXT(glRotatex,void, (GLfixed angle, GLfixed x, GLfixed y, GLfixed z));
 //OGL_EXT(glSampleCoverage,void, (GLclampf value, GLboolean invert));
 //OGL_EXT(glSampleCoveragex,void, (GLclampx value, GLboolean invert));
-OGL_EXT(glScalef,void, (GLfloat x, GLfloat y, GLfloat z));
+//OGL_EXT(glScalef,void, (GLfloat x, GLfloat y, GLfloat z));
 //OGL_EXT(glScalex,void, (GLfixed x, GLfixed y, GLfixed z));
 OGL_EXT(glScissor,void, (GLint x, GLint y, GLsizei width, GLsizei height));
-OGL_EXT(glShadeModel,void, (GLenum mode));
+//OGL_EXT(glShadeModel,void, (GLenum mode));
 OGL_EXT(glStencilFunc,void, (GLenum func, GLint ref, GLuint mask));
 OGL_EXT(glStencilMask,void, (GLuint mask));
 OGL_EXT(glStencilOp,void, (GLenum fail, GLenum zfail, GLenum zpass));
-OGL_EXT(glTexCoordPointer,void, (GLint size, GLenum type, GLsizei stride, const GLvoid *pointer));
-OGL_EXT(glTexEnvf,void, (GLenum target, GLenum pname, GLfloat param));
-OGL_EXT(glTexEnvfv,void, (GLenum target, GLenum pname, const GLfloat *params));
+//OGL_EXT(glTexCoordPointer,void, (GLint size, GLenum type, GLsizei stride, const GLvoid *pointer));
+//OGL_EXT(glTexEnvf,void, (GLenum target, GLenum pname, GLfloat param));
+//OGL_EXT(glTexEnvfv,void, (GLenum target, GLenum pname, const GLfloat *params));
 //OGL_EXT(glTexEnvx,void, (GLenum target, GLenum pname, GLfixed param));
 //OGL_EXT(glTexEnvxv,void, (GLenum target, GLenum pname, const GLfixed *params));
 OGL_EXT(glTexImage2D,void, (GLenum target, GLint level, GLint internalformat, GLsizei width, GLsizei height, GLint border, GLenum format, GLenum type, const GLvoid *pixels));
 OGL_EXT(glTexParameterf,void, (GLenum target, GLenum pname, GLfloat param));
 OGL_EXT(glTexParameteri,void, (GLenum target, GLenum pname, GLint param));
 OGL_EXT(glTexSubImage2D,void, (GLenum target, GLint level, GLint xoffset, GLint yoffset, GLsizei width, GLsizei height, GLenum format, GLenum type, const GLvoid *pixels));
-OGL_EXT(glTranslatef,void, (GLfloat x, GLfloat y, GLfloat z));
+//OGL_EXT(glTranslatef,void, (GLfloat x, GLfloat y, GLfloat z));
 //OGL_EXT(glTranslatex,void, (GLfixed x, GLfixed y, GLfixed z));
-OGL_EXT(glVertexPointer,void, (GLint size, GLenum type, GLsizei stride, const GLvoid *pointer));
+//OGL_EXT(glVertexPointer,void, (GLint size, GLenum type, GLsizei stride, const GLvoid *pointer));
 OGL_EXT(glViewport,void, (GLint x, GLint y, GLsizei width, GLsizei height));
 OGL_EXT(glGetTexParameteriv,void,(GLenum target,  GLenum pname,  GLint * params));
 OGL_EXT(glIsTexture, GLboolean, ( GLuint texture) );
