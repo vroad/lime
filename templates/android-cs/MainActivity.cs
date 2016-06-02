a332bbb604a461671fda819820e3c9f65b1c035a
vroad <xvroad@gmail.com>
2016-06-02 14:13:28 +0900

    Fix android-cs template to handle configChanges and remove OnCreate

--- templates/android-cs/MainActivity.cs
+++ templates/android-cs/MainActivity.cs
@@ -1,11 +1,14 @@
 using Android.App;
+using Android.Content.PM;
 using Android.OS;
 using cs.ndll;
 using Org.Libsdl.App;
 
 namespace ::APP_PACKAGE::
 {
-    [Activity(Label = "::APP_TITLE::", Name="::APP_PACKAGE::.MainActivity", MainLauncher = true, Icon = "@drawable/icon")]
+    [Activity(Label = "::APP_TITLE::", Name="::APP_PACKAGE::.MainActivity",
+        ConfigurationChanges = ConfigChanges.KeyboardHidden | ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.ScreenLayout,
+        MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Org.Haxe.Lime.GameActivity
     {
         class MainRunnable : Java.Lang.Object, Java.Lang.IRunnable
@@ -20,11 +23,6 @@ namespace ::APP_PACKAGE::
                 SDLActivity.NativeInit(arguments);
                 ApplicationMain.main();
             }
-        }
-
-        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
-        {
-            base.OnCreate(savedInstanceState, persistentState);
         }
 
         public override Java.Lang.IRunnable CreateRunnable()
